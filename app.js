7f5ab947778489374e58bde1830b8f35c9b51b10
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-08-27 19:18:54 +0300

    Added base layouts and home page

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const path = require("path");
 const PORT = 8000;
 const app = express();
 
+// Routes middleware
+const mainRoutes = require("./routes/mainRoutes");
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "static")));
+
+app.use(mainRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
