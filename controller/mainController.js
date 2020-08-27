a3ec9d3b47a4da451772ba6ca8cc11763bc6f3f4
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-08-27 20:28:05 +0300

    Contact page

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,3 +1,7 @@
 exports.getHomePage = (req, res, next) => {
   res.render("pages/home");
 };
+
+exports.getContactPage = (req, res, next) => {
+  res.render("pages/contacts");
+};
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -4,5 +4,6 @@ const mainController = require("../controller/mainController");
 const router = express.Router();
 
 router.get("/", mainController.getHomePage);
+router.get("/contacts", mainController.getContactPage);
 
 module.exports = router;
