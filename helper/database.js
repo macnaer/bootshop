c65dbb047c9a170de5a34e87e5d3d3226ea48146
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-09-15 21:09:45 +0300

    Start using ENV variables

--- helper/database.js
+++ helper/database.js
@@ -1,10 +1,17 @@
 const Sequalize = require("sequelize");
+const DBHOST = process.env.DBHOST;
+const DBUSERNAME = process.env.DBUSERNAME;
+const PASSWORD = process.env.PASSWORD;
+const DATABASENAME = process.env.DATABASENAME;
 
-const { DATABASENAME, PASSWORD, USERNAME, HOSTNAME } =  require("./config");
+console.log("HOSTNAME", DBHOST);
+console.log("DBUSERNAME", DBUSERNAME);
+console.log("DATABASENAME", DATABASENAME);
+console.log("PASSWORD", PASSWORD);
 
-const sequalize = new Sequalize(DATABASENAME, USERNAME, PASSWORD, {
+const sequalize = new Sequalize(DATABASENAME, DBUSERNAME, PASSWORD, {
   dialect: "mysql",
-  host: HOSTNAME
+  host: DBHOST,
 });
 
 module.exports = sequalize;
