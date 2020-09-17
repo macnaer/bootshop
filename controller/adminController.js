ff1d7ed9a626bcca14805a0c6903d33205219dcb
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-09-17 18:39:17 +0300

    Admin panel

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,12 +1,112 @@
 const Product = require("../models/product");
 
+exports.getAddProduct = (req, res, next) => {
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  console.log("Log => ", req.body);
+  const title = req.body.title;
+  const price = req.body.price;
+  const sale = req.body.sale;
+  const imageUrl = req.body.imageUrl;
+  const quantity = req.body.quantity;
+  const color = req.body.color;
+  const shortDescription = req.body.shortDescription;
+  const fullDescription = req.body.fullDescription;
+  const brand = req.body.brand;
+  const model = req.body.model;
+  Product.create({
+    title: title,
+    price: price,
+    sale: sale,
+    imageUrl: imageUrl,
+    quantity: quantity,
+    color: color,
+    shortDescription: shortDescription,
+    fullDescription: fullDescription,
+    brand: brand,
+    model: model,
+  })
+    .then((result) => {
+      // console.log(result);
+      console.log("Created Product");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then((product) => {
+      if (!product) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postEditProduct = (req, res, next) => {
+  console.log("Log => ", req.body);
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.sale;
+  const sale = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  Product.findById(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
+    .then((result) => {
+      console.log("UPDATED PRODUCT!");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
+};
+
 exports.getProducts = (req, res, next) => {
   Product.findAll()
     .then((products) => {
       res.render("admin/products", {
         products: products,
-        pageTitle: "All products",
+        pageTitle: "Admin Products",
+        path: "/admin/products",
       });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId)
+    .then((product) => {
+      return product.destroy();
+    })
+    .then((result) => {
+      console.log("DESTROYED PRODUCT");
+      res.redirect("/admin/products");
     })
     .catch((err) => console.log(err));
 };
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,5 +3,10 @@ const adminController = require("../controller/adminController");
 const router = express.Router();
 
 router.get("/admin/products", adminController.getProducts);
+router.get("/admin/add-product", adminController.getAddProduct);
+router.post("/admin/delete-product", adminController.postDeleteProduct);
+router.post("/admin/add-product", adminController.postAddProduct);
+router.post("/admin/edit-product", adminController.postEditProduct);
+router.get("/admin/edit-product/:productId", adminController.getEditProduct);
 
 module.exports = router;
