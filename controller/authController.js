b46a4e3e79d6ce19b9d10a1c8041a0ee0f2fd67c
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-10-13 19:28:03 +0300

    Register new user

--- controller/authController.js
+++ controller/authController.js
@@ -31,3 +31,26 @@ exports.getRegister = (req, res, next) => {
     isAuthenticated: false,
   });
 };
+
+exports.postRegister = (req, res, next) => {
+  const { username, email, password, confirm_password } = req.body;
+  if (password === confirm_password) {
+    User.findOne({ email: email })
+      .then((user) => {
+        if (user) {
+          return res.redirect("/login");
+        }
+        const newUser = new User({
+          name: username,
+          email: email,
+          password: password,
+          cart: { items: [] },
+        });
+        return newUser.save();
+      })
+      .then((result) => {
+        res.redirect("/login");
+      })
+      .catch((err) => console.log(err));
+  }
+};
